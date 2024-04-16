566433ed1499efeb6d0a9ef65a5a4d8ebbef27ee
David  Hodgson <lshdh2@loginhpc.localdomain>
2024-04-16 11:15:38 +0100

    pratice

--- hpc/nih_2024/nih_2024_model.R
+++ hpc/nih_2024/nih_2024_model.R
@@ -31,6 +31,9 @@ copFuncForm <- function(inf_status, esttitreExp, params) {
 
 copLogLikelihood <- function(inf_status, esttitreExp, params) {
     # COP parameters
+    beta0 <- params[1]
+    beta1 <- params[2]
+    p <- 1.0 / (1.0 + exp(- (beta0 + beta1 * esttitreExp) ) )
     p <- copFuncForm(inf_status, esttitreExp, params)
     ll <- inf_status * log(p) + (1 - inf_status) * log(1 - p)
     ll
---
+++ hpc/nih_2024/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=nih_2024_h3
+#SBATCH --nodes=1
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=david.hodgson@lshtm.ac.uk 
+#SBATCH --ntasks=4
+#SBATCH --mem=8G              # Request 8 GB of RAM
+#SBATCH --time=12:00:00       # Set runtime to 12 hours
+#SBATCH --output=nih_2024_h3_%j.log 
+
+# Load any necessary modules
+#module load R/4.1.2 
+module load gnu9/9.4.0
+module load boost/1.76.0
+
+source activate ~/miniconda3/envs/R
+
+# Change to the directory where your job script is located
+~/miniconda3/envs/R/bin/Rscript nih_2024_run.R
