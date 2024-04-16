a9b6912131a0bf07cb328c24b344c0970444448d
dchodge <david_hodgson1991@hotmail.co.uk>
2024-04-16 09:31:57 +0100

    added in nih_2024 for hpc

---
+++ hpc/nih_2024/nih_2024_run.R
@@ -0,0 +1,27 @@
+library(devtools)
+
+library(Rcpp)
+devtools::load_all()
+
+seroModel <- readRDS(here::here("hpc", "nih_2024", "nih_2024_model.RData"))
+
+settings <-  list(
+    numberChainRuns = 4,
+    numberCores = 4,
+    iterations = 500000,
+    burninPosterior = 250000,
+    thin = 1000,
+    consoleUpdates = 100,
+    onAdaptiveCov = TRUE,
+    updatesAdaptiveCov = 10,
+    burninAdaptiveCov = 1000,
+    covarInitVal = 1e-2, # make very small if struggling to sample to beginning
+    covarInitValAdapt = 1e-2, # make very small if struggling to sample to beginning
+    covarMaxVal = 1, # decrease if struggling toc sample in the middle
+    runParallel = TRUE,
+    noGibbsSteps = 1,
+    onDebug = FALSE
+)
+
+runRJMCMC(seroModel, settings, "hpc/nih_2024", "h3")
+postprocessFigs("hpc/nih_2024", "h3", 4)
