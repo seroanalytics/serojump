dbef699310280d0bf48c0451bf720f8476c8f8ad
David  Hodgson <lshdh2@loginhpc.localdomain>
2024-04-23 13:57:55 +0100

    ran on Tue Apr 23 13:57:55 BST 2024

--- hpc/nih_2024/run.sh
+++ hpc/nih_2024/run.sh
@@ -3,10 +3,11 @@
 #SBATCH --nodes=1
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=david.hodgson@lshtm.ac.uk 
-#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=4
 #SBATCH --mem=8G              # Request 8 GB of RAM
 #SBATCH --time=48:00:00       # Set runtime to 12 hours
 #SBATCH --output=nih_2024_h3_%j.log 
+#SBATCH --array=1-2
 
 # Load any necessary modules
 #module load R/4.1.2 
@@ -16,4 +17,4 @@ module load boost/1.76.0
 source activate ~/miniconda3/envs/R
 
 # Change to the directory where your job script is located
-~/miniconda3/envs/R/bin/Rscript nih_2024_run.R
+~/miniconda3/envs/R/bin/Rscript nih_2024_run.R $SLURM_ARRAY_TASK_ID
