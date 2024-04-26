323f37f76f1192087eef0a9af1b3dde7ccf89fa7
David  Hodgson <lshdh2@loginhpc.localdomain>
2024-04-26 09:18:57 +0100

    ran on Fri Apr 26 09:18:56 BST 2024

--- hpc/simstudy/run.sh
+++ hpc/simstudy/run.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51c4e0c84ff69ee4339ab833ea6266ac7f1d6bb7d0b8e122c7cf4b895725b7a8
-size 768
+oid sha256:eca6f78f4dd330b27d4ae84d15cf41e153f941915afd9e33be249b716e59c10a
+size 767
